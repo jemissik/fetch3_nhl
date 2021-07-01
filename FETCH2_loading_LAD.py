debd80a3e0fe8f19864aa562dddba9da8662498d
Justine Missik <missik.2@osu.edu>
2021-07-01 11:29:35 -0400

    Moved parameter definitions to FETCH2_config file

--- FETCH2_loading_LAD.py
+++ FETCH2_loading_LAD.py
@@ -35,70 +35,6 @@ df_verma=df_verma.loc[:'2007-03-01 00:00:00']
 Precipitation=df_rain.values
 Precipitation=Precipitation.reshape(len(Precipitation))
 
-params={}
-
-#OTHER PARAMETERS
-params['Rho']=1000       #[kg/m3]
-params['g']=9.8          #[m/s2]
-
-#SOIL PARAMETERS - USING VAN GENUCHTEN 
-
-#CLAY
-params['alpha_1']=0.8                        #soil hydraulic parameter [1/m]
-params['theta_S1']=0.55                      #saturated volumetric soil moisture content [-]
-params['theta_R1']=0.068                     #residual volumetric soil moisture content [-]
-params['n_1']=1.5                            #soil hydraulic parameter  [-]
-params['m_1']=1-(1/params['n_1'])            #soil hydraulic parameter  [-]
-params['Ksat_1']=1.94*10**(-7)               #saturated hydraulic conductivity  [m/s]
-
-#SAND
-params['alpha_2']=14.5
-params['theta_S2']=0.47
-params['theta_R2']=0.045
-params['n_2']=2.4
-params['m_2']=1-(1/params['n_2'])
-params['Ksat_2']=3.45*10**(-5)
-
-
-#ROOT PARAMETERS 
-#diving by Rho*g since Richards equation is being solved in terms of \Phi (Pa)
-params['Kr']=(7.2*10**(-10))/(params['Rho']*params['g']) #soil-to-root radial conductance [m/sPa]                                                          
-params['qz']=9                                           #unitless - parameter for the root mass distribution - Verma et al., 2014
-params['Ksax']=(10**(-5))/(params['Rho']*params['g'])    #specific axial conductivity of roots  [ m/s]
-params['Aind_r']=1                                       #m2 root xylem/m2 ground]  
-
-
-#XYLEM PARAMETERS
-params['kmax']=(10**(-5))/(params['Rho']*params['g'])    #conductivity of xylem  [ m/s]
-params['ap']=2*10**(-6)                                  #xylem cavitation parameter [Pa-1]
-params['bp']=-1.5*10**(6)                                #xylem cavitation parameter [Pa]
-params['Aind_x']=8.62*10**(-4)                           #m2 xylem/m2 ground]  
-params['Phi_0']=5.74*10**8                               #From bohrer et al 2005       
-params['p']=20                                           #From bohrer et al 2005
-params['sat_xylem']=0.573                                #From bohrer et al 2005 
-####################PENMAN-MONTEITH EQUATION PARAMETERS
-#W m^-2 is the same as J s^-1 m^-2
-#1J= 1 kg m2/s2
-#therefore 1W/m2 = kg/s3
-
-kr=5*10**(-3)         #m2/W Jarvis radiation parameter
-kt=1.6*10**(-3)       #K-2  Jarvis temperature parameter
-Topt=289.15           #K   Jarvis temperature parameter (optimum temperature)
-kd=1.1*10**(-3)       #Pa-1 Jarvis vapor pressure deficit temperature
-hx50=-1274000         #Pa  Jarvis leaf water potential parameter
-
-
-
-nl=2                   #[-] Jarvis leaf water potential parameter
-gsmax=10*10**(-3)      #m/s Maximum leaf stomatal conductance
-gb=2*10**(-2)          #m/s Leaf boundary layer conductance
-LAI=1.5                #[-] Leaf area index
-Cp=1200                # J/m3 K Heat capacity of air
-ga=2*10**(-2)          #m/s Aerodynamic conductance
-lamb=2.51*10**9        #J/m3 latent heat of vaporization 
-gama=66.7              #Pa/K psuchrometric constant
-Emax=1*10**(-9)        #m/s maximum nightime transpiration
-
 
 #Interpolating input variables for PM transpiration
 
@@ -131,11 +67,6 @@ t_data=list(t_data)
 nt_data=len(t_data)                      #length of input data   
 
 ###############################################################################
-
-#TREE PARAMETERS
-params['Hspec']=14                      #Height average of trees [m]
-params['LAI']=1.5                       #[-] Leaf area index                 
-params['Abasal']=8.62*10**(-4)          #[m2basal/m2-ground] xylem cross-sectional area and site surface ratio
 
 
 ################### SOIL Boundary conditions
