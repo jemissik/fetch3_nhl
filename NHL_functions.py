Change to run in FETCH3 - Runs in FETCH3 (but not tested yet) - changed zmin to 0 rather than the lowest z point in the LAD data - Add LAD to output - Corrected issue with sign of water potential in the wp_response function - Change calc_transpiration to multipy by LAD - Change paths in main for running from FETCH3 rather than NHL directory

NHL_functions.py
@@ -610,7 +610,8 @@ def calc_LAI_vertical(LADnorm, z_h_LADnorm, tot_LAI_crown, dz, h): #TODO make su
     """
     z_LAD = z_h_LADnorm * h  # Heights for LAD points
     dz_LAD = z_LAD[1] - z_LAD[0]
-    zmin = z_LAD[0]
+    # zmin = z_LAD[0]
+    zmin = 0 #TODO
     z = np.arange(zmin, h, dz)  # New array for vertical resolution
 
     #Calculate LAD
@@ -621,7 +622,7 @@ def calc_LAI_vertical(LADnorm, z_h_LADnorm, tot_LAI_crown, dz, h): #TODO make su
     spl = splrep(z_LAD, LADnorm)
     LADnorm_z = splev(z, spl)  # LAD interpolated to new vertical grid
     '''
-    f = interp1d(z_LAD, LAD)
+    f = interp1d(z_LAD, LAD, bounds_error = False, fill_value='extrapolate')
     LAD_z = f(z)
 
     # scale so new integrated LAD matches the original total LAI per crown (corrects for interpolation error)
@@ -665,7 +666,8 @@ def calc_NHL(dz, h, Cd, U_top, ustar, PAR, Ca, Vcmax25, alpha_gs, alpha_p, total
     VPD = calc_vpd_kPa(RH, Tair = Tair)
 
     #Set up vertical grid
-    zmin = z_h_LADnorm[0] * h  # [m]
+    # zmin = z_h_LADnorm[0] * h  # [m]
+    zmin = 0 #TODO
     z = np.arange(zmin, h, dz)  # [m]
 
     # Calculate leaf area for each vertical layer (for one tree)
@@ -732,13 +734,14 @@ def calc_NHL(dz, h, Cd, U_top, ustar, PAR, Ca, Vcmax25, alpha_gs, alpha_p, total
         attrs=dict(description="Model output")
         )
 
-    return ds, NHL_tot_trans_sp_tree, zenith_angle
+    return ds, NHL_tot_trans_sp_tree, LAD, zenith_angle
 
 def calc_NHL_timesteps(dz, h, Cd, met_data, Vcmax25, alpha_gs, alpha_p,
             total_LAI_spn, plot_area, total_crown_area_spn, mean_crown_area_spn, LAD_norm, z_h_LADnorm,
             lat, long, time_offset = -5):
 
-    zmin = z_h_LADnorm[0] * h  # [m]
+    # zmin = z_h_LADnorm[0] * h  # [m]
+    zmin = 0 #TODO
     z = np.arange(zmin, h, dz)  # [m]
 
     NHL_tot_trans_sp_tree_all = np.empty((len(met_data)))
@@ -749,7 +752,7 @@ def calc_NHL_timesteps(dz, h, Cd, met_data, Vcmax25, alpha_gs, alpha_p,
     for i in range(0,len(met_data)):
         if i%50==0:
             print('Calculating step ' + str(i))
-        ds, NHL_tot_trans_sp_tree, zenith_angle = calc_NHL(
+        ds, NHL_tot_trans_sp_tree, LAD, zenith_angle = calc_NHL(
             dz, h, Cd, met_data.WS_F.iloc[i], met_data.USTAR.iloc[i], met_data.PPFD_IN.iloc[i], met_data.CO2_F.iloc[i], Vcmax25, alpha_gs, alpha_p,
             total_LAI_spn, plot_area, total_crown_area_spn, mean_crown_area_spn, LAD_norm, z_h_LADnorm,
             met_data.RH.iloc[i], met_data.TA_F.iloc[i], met_data.PA_F.iloc[i], doy = met_data.Timestamp.iloc[i].dayofyear, lat = lat,
@@ -759,7 +762,7 @@ def calc_NHL_timesteps(dz, h, Cd, met_data, Vcmax25, alpha_gs, alpha_p,
         zenith_angle_all[i] = zenith_angle
         datasets.append(ds)
     d2 = xr.concat(datasets, pd.Index(met_data.Timestamp, name="time"))
-    return d2, NHL_tot_trans_sp_tree_all, zenith_angle_all
+    return d2, NHL_tot_trans_sp_tree_all, LAD, zenith_angle_all
 
 def calc_stem_wp_response(stem_wp, wp_s50, c3):
     """
@@ -770,10 +773,10 @@ def calc_stem_wp_response(stem_wp, wp_s50, c3):
     Parameters
     ----------
     stem_wp : [type]
-        Stem water pressure (function of z and time)
+        Stem water potential (function of z and time) [Pa]
     wp_s50 : [type]
         Empirical shape parameter describing the inflection point
-        of the leaf stem water potential response curve
+        of the leaf stem water potential response curve [Pa]
     c3 : [type]
         Shape parameter for stomatal response
 
@@ -782,11 +785,11 @@ def calc_stem_wp_response(stem_wp, wp_s50, c3):
     [type]
         [description]
     """
-    wp_response = np.exp(-((-stem_wp)/wp_s50)**c3)
+    wp_response = np.exp(-((stem_wp)/wp_s50)**c3)  # TODO: deleted - in front of stem_wp, assuming eqn in paper was using positive water potential
     return wp_response
 
-def calc_transpiration_nhl(nhl_transpiration, stem_wp_fn):
-    return nhl_transpiration * stem_wp_fn
+def calc_transpiration_nhl(nhl_transpiration, stem_wp_fn, LAD):
+    return nhl_transpiration * stem_wp_fn * LAD
 
 def write_outputs(output_vars):
 

main.py
@@ -7,8 +7,10 @@ import matplotlib.pyplot as plt
 import xarray as xr
 from pathlib import Path
 
-import nhl_config as ncfg
-from NHL_functions import *
+import nhl_transpiration.nhl_config as ncfg
+# from NHL_functions import *
+from nhl_transpiration.NHL_functions import *
+
 import time
 start = time.time()
 
@@ -16,10 +18,10 @@ start = time.time()
 
 # Read in LAD and met data
 
-# met_data = pd.read_csv(Path.cwd() / 'nhl_transpiration/data' / ncfg.met_data)
-# LAD_data = pd.read_csv(Path.cwd() / 'nhl_transpiration/data' / ncfg.LAD_norm)
-met_data = pd.read_csv(Path.cwd() / 'data' / ncfg.met_data, parse_dates=[0]) #TODO parse timestamps for met data
-LAD_data = pd.read_csv(Path.cwd() / 'data' / ncfg.LAD_norm)
+met_data = pd.read_csv(Path.cwd() / 'nhl_transpiration/data' / ncfg.met_data, parse_dates=[0])
+LAD_data = pd.read_csv(Path.cwd() / 'nhl_transpiration/data' / ncfg.LAD_norm)
+# met_data = pd.read_csv(Path.cwd() / 'data' / ncfg.met_data, parse_dates=[0])
+# LAD_data = pd.read_csv(Path.cwd() / 'data' / ncfg.LAD_norm)
 
 met_data = met_data[(met_data.Timestamp >= pd.to_datetime(ncfg.start_time)) &
                     (met_data.Timestamp <= pd.to_datetime(ncfg.end_time))].reset_index(drop=True)
@@ -34,13 +36,13 @@ total_crown_area_sp = total_LAI_sp * crown_scaling / sum(total_LAI_sp * crown_sc
 # met_data = met_data.loc[7218:7218]
 
 
-ds, tot_trans, zen = calc_NHL_timesteps(ncfg.dz, ncfg.height_sp, ncfg.Cd, met_data, ncfg.Vcmax25, ncfg.alpha_gs, ncfg.alpha_p,
+ds, tot_trans, LAD, zen = calc_NHL_timesteps(ncfg.dz, ncfg.height_sp, ncfg.Cd, met_data, ncfg.Vcmax25, ncfg.alpha_gs, ncfg.alpha_p,
             total_LAI_sp[0], ncfg.plot_area, total_crown_area_sp[0], ncfg.mean_crown_area_sp, LAD_data[ncfg.species], LAD_data.z_h,
             ncfg.latitude, ncfg.longitude, time_offset = ncfg.time_offset)
 
 
 write_outputs_netcdf(ds)
-write_outputs({'tot_trans':tot_trans, 'zenith':zen})
+write_outputs({'tot_trans':tot_trans, 'zenith':zen, 'LAD': LAD})
 
 df = pd.DataFrame(data={'tot_trans':tot_trans, 'zenith':zen, 'Timestamp': met_data.Timestamp} )
 
@@ -54,7 +56,9 @@ ds2 = ds.assign_coords({'time': pd.to_timedelta(pd.to_datetime(ds.time.values) -
 # model_ts = np.arange(ds2.time.values[0], ds2.time.values[-1] +  + ncfg.met_dt, ncfg.dt0)
 model_ts = np.arange(0, len(ds.time) * ncfg.met_dt + ncfg.dt0, ncfg.dt0)
 
-model_z = np.arange(0, ncfg.height_sp + ncfg.dz, ncfg.dz)
+# model_z = np.arange(0, ncfg.height_sp + ncfg.dz, ncfg.dz)
+model_z = np.arange(0, ncfg.height_sp, ncfg.dz)
+
 
 #NHL transpiration in units of m s-1 * LAD  = kg H2O s-1 m-1stem m-2ground
 da = ds2.NHL_trans_sp_stem * 10**-3 #NHL in units of m s-1 * m-1stem
@@ -64,4 +68,5 @@ NHL_modelres = da.interp(z = model_z, time = model_ts, assume_sorted = True, kwa
 NHL_modelres.to_netcdf('output/nhl_modelres_trans_out.nc')
 
 NHL_modelres = NHL_modelres.data.transpose()
+
 print(f"run time: {time.time() - start} s")  # end run clock