d228445ad978b9525e896e9a3c559444295ffd47
Justine Missik <missik.2@osu.edu>
2022-02-02 18:11:28 -0500

    Updates met data to work for both PM and NHL, update NHL output units

--- met_data.py
+++ met_data.py
@@ -15,7 +15,25 @@ def calc_infiltration_rate(precipitation, tmax, dt0):
     return q_rain
 def interp_to_model_res(var, tmax, dt0):
     return np.interp(np.arange(0, tmax + dt0, dt0), t_data, var)
+def calc_esat(Ta):
+    return 611*np.exp((17.27*(Ta-273.15))/(Ta-35.85)) #Pascal
+def calc_delta(Ta, e_sat):
+    return (4098/((Ta-35.85)**2))*e_sat
+def calc_NETRAD(SW_in):
+    """
+    Calculate net radiation as 60% of total incoming solar radiation
 
+    Parameters
+    ----------
+    SW_in : [W m-2]
+        Total incoming solar radiation
+
+    Returns
+    -------
+    [W m-2]
+        Net radiation
+    """
+    return SW_in * 0.6
 ###########################################################
 #Load and format input data
 ###########################################################
@@ -42,9 +60,41 @@ nt_data=len(t_data)                      #length of input data
 #variables to arrays
 precipitation = df['P_F'].values
 
+Ta_C = df['TA_F']
+SW_in = df['SW_IN_F']
+VPD = df['VPD_kPa']
+
+#temperature
+Ta = Ta_C + 273.15 #converting temperature from degree Celsius to Kelvin
+Ta = Ta.interpolate(method = 'linear')
+
+#incoming solar radiation
+SW_in = SW_in.interpolate(method = 'time')
+
+#vapor pressure deficit
+VPD=VPD[VPD > 0] #eliminating negative VPD
+VPD = VPD.reindex(SW_in.index)
+VPD=VPD.interpolate(method='linear')*1000  #kPa to Pa
+VPD=VPD.fillna(0)
+
 ########################################################
 #SETTING PRECIPITATION AS INFILTRATION BOUNDARY CONDITION
 #in case of set by user
 ###########################################################
 
 q_rain = calc_infiltration_rate(precipitation, tmax, cfg.dt0)
+
+Ta = interp_to_model_res(Ta, tmax, cfg.dt0)
+SW_in = interp_to_model_res(SW_in, tmax, cfg.dt0)
+VPD = interp_to_model_res(VPD, tmax, cfg.dt0)
+
+e_sat = calc_esat(Ta)
+delta_2d = calc_delta(Ta, e_sat)
+
+NET = calc_NETRAD(SW_in)
+
+####2d interpolation of met data
+NET_2d = interpolate_2d(NET, len(z_upper))
+VPD_2d = interpolate_2d(VPD, len(z_upper))
+Ta_2d = interpolate_2d(Ta, len(z_upper))
+SW_in_2d = interpolate_2d(SW_in, len(z_upper))
