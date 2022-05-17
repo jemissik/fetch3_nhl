42122a188364bae5e3333e6ef26f008e42c624ae
Justine Missik <missik.2@osu.edu>
2022-05-17 17:35:07 -0400

    Update run_debug with more explanation

--- run_debug.py
+++ run_debug.py
@@ -1,10 +1,22 @@
+"""
+Convenience script for running the model in a debugger (i.e., when you don't want
+to run the model from the command line).
+Here, you can pass config_path, data_path, and output_path directly rather than as
+command line arguments.
+The paths here are for the default locations, but you can change these if you want to use
+different files or directories.
+"""
+
 from pathlib import Path
 from fetch3.__main__ import run
 
-
+# These paths point to the default locations
 parent_path = Path(__file__).resolve().parent
-default_config_path = parent_path / 'config_files' / 'model_config.yml'
-default_data_path = parent_path / 'data'
-default_output_path = parent_path / 'output'
+config_path = parent_path / 'config_files' / 'model_config.yml'
+data_path = parent_path / 'data'
+output_path = parent_path / 'output'
 
-run(default_config_path, default_data_path, default_output_path)
+# If you want to use different files or directories, you can change these paths, for example:
+# config_path = Path("/Users/jmissik/Desktop/repos/fetch3_nhl/config_files/opt_umbs_M8.yml")
+
+run(config_path, data_path, output_path)
