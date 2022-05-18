Fixed issues caused by floating point precision error in z-axis discretization

fetch3/model_setup.py
@@ -19,19 +19,20 @@ def spatial_discretization(dz, Soil_depth, Root_depth, Hspec, sand_d, clay_d):
     #below-ground spatial discretization
     #######################################
     zmin=0     #[m] minimum depth of soil [bottom of soil]
-    z_soil=np.arange(zmin,Soil_depth + dz, dz)
+    z_soil=np.around(np.arange(zmin,Soil_depth + dz, dz), decimals=5) # Rounding to get rid of floating point precision error
     nz_s=len(z_soil)
 
     #measurements depths of soil [m]
-    z_root=np.arange((Soil_depth - Root_depth) + zmin, Soil_depth + dz, dz)
+    z_root= np.around(np.arange((Soil_depth - Root_depth) + zmin, Soil_depth + dz, dz), decimals=5)
     nz_r=len(z_soil)+len(z_root)
 
     #############################################
     #above-ground spatial discretization
     #################################################
-    z_Above=np.arange(zmin, Hspec + dz, dz)  #[m]
+    z_Above=np.around(np.arange(zmin, Hspec + dz, dz), decimals=5)  #[m]
     nz_Above=len(z_Above)
-    z_upper=np.arange((z_soil[-1] + dz),(z_soil[-1] + Hspec + dz), dz)
+
+    z_upper=np.around(np.arange((z_soil[-1] + dz),(z_soil[-1] + Hspec + dz), dz), decimals=5)
 
     z=np.concatenate((z_soil,z_root,z_upper))
 
@@ -41,8 +42,8 @@ def spatial_discretization(dz, Soil_depth, Root_depth, Hspec, sand_d, clay_d):
     #CONFIGURATION OF SOIL DUPLEX
     #depths of layer/clay interface
     #####################################################################
-    nz_sand=int(np.flatnonzero(z==sand_d)) #node where sand layer finishes
-    nz_clay=int(np.flatnonzero(z==clay_d)) #node where clay layer finishes- sand starts
+    nz_sand=int(np.flatnonzero(z_soil==sand_d)) #node where sand layer finishes
+    nz_clay=int(np.flatnonzero(z_soil==clay_d)) #node where clay layer finishes- sand starts
 
     zind = Zind(z_soil=z_soil,
                 nz_s=nz_s,