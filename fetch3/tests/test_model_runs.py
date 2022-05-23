a926029baab2ddee4393ff8ff703764c3a825ad4
Justine Missik <missik.2@osu.edu>
2022-05-23 13:49:32 -0400

    Add additional tests: - fetch3 run using PM transpiration scheme - optimization run

--- fetch3/tests/test_model_runs.py
+++ fetch3/tests/test_model_runs.py
@@ -1,13 +1,33 @@
 import pytest
 from pathlib import Path
 
-def test_fetch3_nhl_run():
-    config_path = Path(__file__).resolve().parent.parent.parent / 'config_files' / 'test_nhl_oak.yml'
+# def test_fetch3_nhl_run():
+#     config_path = Path(__file__).resolve().parent / 'config_files' / 'test_nhl_oak.yml'
+#     data_path = Path(__file__).resolve().parent.parent.parent / 'data'
+#     output_path = Path(__file__).resolve().parent.parent.parent / 'output'
+#     from fetch3.__main__ import run
+#     run(config_path, data_path, output_path)
+
+
+def test_fetch3_nhl_run_optconfig():
+    config_path = Path(__file__).resolve().parent / 'config_files' / 'opt_umbs_M8.yml'
     data_path = Path(__file__).resolve().parent.parent.parent / 'data'
     output_path = Path(__file__).resolve().parent.parent.parent / 'output'
+    print(output_path)
     from fetch3.__main__ import run
     run(config_path, data_path, output_path)
 
-# /Users/jmissik/Desktop/repos/fetch3_nhl/output
+def test_fetch3_PM_run():
+    config_path = Path(__file__).resolve().parent / 'config_files' / 'test_PM.yml'
+    data_path = Path(__file__).resolve().parent.parent.parent / 'data'
+    output_path = Path(__file__).resolve().parent.parent.parent / 'output'
+    print(output_path)
+    from fetch3.__main__ import run
+    run(config_path, data_path, output_path)
 
-# /Users/jmissik/Desktop/repos/fetch3_nhl/output
+def test_fetch3_opt_run():
+    config_path = Path(__file__).resolve().parent.parent.parent / 'config_files' / 'opt_umbs_M8.yml'
+    output_path = Path(__file__).resolve().parent.parent.parent / 'output'
+    print(output_path)
+    from fetch3.optimize.run_optimization import run
+    run(config_path)
