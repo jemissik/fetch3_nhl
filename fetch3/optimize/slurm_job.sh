208852a53a49f2b7ddc865a990688a96088bcd51
Justine Missik <missik.2@osu.edu>
2022-06-17 15:01:07 -0400

    Update slurm script

--- fetch3/optimize/slurm_job.sh
+++ fetch3/optimize/slurm_job.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=test_job
-#SBATCH --time=15:00
-#SBATCH --nodes=1 --ntasks-per-node=5
-#SBATCH --partition=parallel
+#SBATCH --time=60:00
+#SBATCH --nodes=1 --ntasks-per-node=12
+#SBATCH --output=logs/%j.log
 #SBATCH --account=PAS0409
 
 set -x  # for displaying the commands in the log for debugging
 
-config_dir=$SLURM_SUBMIT_DIR/umbs_optimization_config.yml
+config_dir=$SLURM_SUBMIT_DIR/opt_umbs_M8.yml
 
 
 echo $config_dir
