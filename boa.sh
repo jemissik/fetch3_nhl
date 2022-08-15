d9654b1a8fd770a277940140982f95b963560bfc
Madeline Scyphers <madelinescyphers@gmail.com>
2022-08-15 00:51:43 -0400

    batch: modify batch scripts to be directory structure agnostic

--- boa.sh
+++ boa.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 #SBATCH --job-name=boa
-#SBATCH --time=7:50:00
+#SBATCH --time=2:00:00
 #SBATCH --nodes=1 --ntasks-per-node=14
 #SBATCH --output=output/boa_%j.log
 #SBATCH --account=PAS0409
 
 set -x  # for displaying the commands in the log for debugging
 
-config_file=$SLURM_SUBMIT_DIR/config_files/opt_umbs_birch_avg.yml
+config_file=$1
 
-
+echo `pwd`
 echo $config_file
 
 source activate fetch3-dev
-
-cd ~/fetch3_nhl
 
 python optimization_run.py --config_file $config_file -b
 
--- slurm_main.sh
+++ slurm_main.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=boa_%j
-#SBATCH --time=7:50:00
+#SBATCH --job-name=main
+#SBATCH --time=50:00
 #SBATCH --nodes=1 --ntasks-per-node=28
-#SBATCH --output=output/boa_%j.log
+#SBATCH --output=output/boa_main_%j.log
 #SBATCH --account=PAS0409
 
 set -x  # for displaying the commands in the log for debugging
@@ -10,13 +10,11 @@ set -x  # for displaying the commands in the log for debugging
 config_path=$1
 data_path=$2
 output_path=$3
-working_dir=/users/PAS0409/madelinescyphers/fetch3_nhl
 
+echo `pwd`
 echo $config_file
 
 source activate fetch3-dev
-
-cd $working_dir
 
 python main.py --config_path $config_path --data_path $data_path --output_path $output_path
 
