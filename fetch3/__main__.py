48b8c57f0631387e9b390d8e95205f5685dbb3cb
Justine Missik <missik.2@osu.edu>
2023-02-18 17:51:18 -0500

    Update dunder main file with comment about why it's needed

--- fetch3/__main__.py
+++ fetch3/__main__.py
@@ -1,3 +1,7 @@
+# Having this __main__.py file in addition to run.py is a workaround for weirdness with the multiprocessing module
+# Multiprocessing module has issues with having everything in the __main__ file
+
+
 from fetch3.run import main
 
 
