bfb213102657f2e7810e7c98d9c29d125a71d5a0
Justine Missik <missik.2@osu.edu>
2023-06-22 11:08:43 -0400

    make interpolation to model res in nhl optional

--- fetch3/nhl_transpiration/main.py
+++ fetch3/nhl_transpiration/main.py
@@ -23,14 +23,10 @@ from fetch3.met_data import prepare_ameriflux_data
 from fetch3.nhl_transpiration.NHL_functions import *
 
 
-def main(cfg, output_dir, data_dir):
+def main(cfg, output_dir, data_dir, to_model_res=True):
     logger = logging.getLogger(__name__)
 
     start = time.time()
-
-    ####################################################
-    # The rest of the file should be the same as main.py
-    ###################################################
 
     # Read in LAD and met data
     met_data = prepare_ameriflux_data(data_dir / cfg.input_fname, cfg)
@@ -50,36 +46,39 @@ def main(cfg, output_dir, data_dir):
 
     logger.info(f"NHL calculations finished in {time.time() - start} s")
 
+    if not to_model_res:
     # logger.info("Saving NHL output...")
-    # write_outputs_netcdf(output_dir, ds)
-    # write_outputs({"zenith": zen, "LAD": LAD}, output_dir)
+        write_outputs_netcdf(output_dir, ds)
+        write_outputs({"zenith": zen, "LAD": LAD}, output_dir)
+        return ds, LAD
 
-    logger.info(f"Interpolating NHL to the time resolution for FETCH3...")
-    # Interpolate to model time resolution
-    # time in seconds
-    ds2 = ds.assign_coords(
-        {
-            "time": pd.to_timedelta(
-                pd.to_datetime(ds.time.values) - pd.to_datetime(ds.time.values[0])
-            )
-            / np.timedelta64(1, "s")
-        }
-    )
+    if to_model_res:
+        logger.info(f"Interpolating NHL to the time resolution for FETCH3...")
+        # Interpolate to model time resolution
+        # time in seconds
+        ds2 = ds.assign_coords(
+            {
+                "time": pd.to_timedelta(
+                    pd.to_datetime(ds.time.values) - pd.to_datetime(ds.time.values[0])
+                )
+                / np.timedelta64(1, "s")
+            }
+        )
 
-    # New time and space coordinates matching model resolution
-    model_ts = np.arange(0, len(ds.time) * cfg.dt + cfg.dt0, cfg.dt0)
-    model_z = np.arange(0, cfg.Hspec, cfg.dz)
+        # New time and space coordinates matching model resolution
+        model_ts = np.arange(0, len(ds.time) * cfg.dt + cfg.dt0, cfg.dt0)
+        model_z = np.arange(0, cfg.Hspec, cfg.dz)
 
-    da = (
-        ds2.NHL_trans_sp_stem * 10**-3
-    )  # NHL in units of kg m-2crown m-1stem s-1 #* 10**-3 to convert kg to m
+        da = (
+            ds2.NHL_trans_sp_stem * 10**-3
+        )  # NHL in units of kg m-2crown m-1stem s-1 #* 10**-3 to convert kg to m
 
-    NHL_modelres = da.interp(z=model_z, time=model_ts, assume_sorted=True, kwargs={"fill_value": 0})
+        NHL_modelres = da.interp(z=model_z, time=model_ts, assume_sorted=True, kwargs={"fill_value": 0})
 
-    # logger.info("Saving NHL_modelres output...")
-    # # write NHL output to netcdf
-    # NHL_modelres.to_netcdf(output_dir / "nhl_modelres_trans_out.nc")
-    NHL_modelres = NHL_modelres.data.transpose()
+        # logger.info("Saving NHL_modelres output...")
+        # # write NHL output to netcdf
+        # NHL_modelres.to_netcdf(output_dir / "nhl_modelres_trans_out.nc")
+        NHL_modelres = NHL_modelres.data.transpose()
 
-    logger.info(f"NHL module finished in {time.time() - start} s")
-    return NHL_modelres, LAD
+        logger.info(f"NHL module finished in {time.time() - start} s")
+        return NHL_modelres, LAD
