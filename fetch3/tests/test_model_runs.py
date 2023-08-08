4b677a3ba4e11a410f94abf0b5cd6761d7564053
Justine Missik <missik.2@osu.edu>
2023-08-08 15:54:51 -0400

    add test for running nhl in standalone mode

--- fetch3/tests/test_model_runs.py
+++ fetch3/tests/test_model_runs.py
@@ -59,3 +59,19 @@ def test_fetch3_opt_run():
         Path(__file__).resolve().parent.parent.parent / "config_files" / "opt_model_config.yml"
     )
     dunder_main.main(split_shell_command(f"--config-path {config_path} -td"), standalone_mode=False)
+
+
+def test_nhl_standalone_run():
+    config_path = (
+        Path(__file__).resolve().parent.parent.parent / "config_files" / "model_config.yml"
+    )
+    data_path = Path(__file__).resolve().parent.parent.parent / "data"
+    output_path = Path(__file__).resolve().parent.parent.parent / "output"
+
+    with open(config_path, "r") as yml_config:
+        loaded_configs = yaml.safe_load(yml_config)
+        species_list = list(loaded_configs['species_parameters'].keys())
+
+    from fetch3.nhl_transpiration.main_standalone import run
+
+    run(["--config_path", config_path, "--data_path", data_path, "--species", species_list[0]], standalone_mode=False)
