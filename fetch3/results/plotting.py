8cc01ddb1cf229bb874e2c3ee332b5e4ba2b3509
Justine Missik <missik.2@osu.edu>
2023-11-14 14:35:39 -0500

    Update plotting functions

--- fetch3/results/plotting.py
+++ fetch3/results/plotting.py
@@ -3,7 +3,23 @@ import hvplot.xarray
 
 import holoviews as hv
 from holoviews import dim, opts
-from fetch3.results.results import Results
+# from fetch3.results.results import Results
+
+hv.plotting.bokeh.ElementPlot.width = 1200
+hv.plotting.bokeh.ElementPlot.aspect = 4
+hv.plotting.bokeh.ElementPlot.fontscale = 1.5
+
+plot_kw = dict(
+    responsive=True,
+               tools=['hover'],
+            aspect=4,
+            active_tools=['box_zoom'],
+            width=1200,
+            fontscale=1.5,
+              )
+
+def xlabel_off(plot, element):
+    plot.state.xaxis.major_label_text_alpha = 0
 
 
 def plot_2d_slice(ds=None, z=None, var=None):
@@ -13,6 +29,8 @@ def plot_2d_slice(ds=None, z=None, var=None):
 
 def plot_precip_and_swc(model_ds, z=None, obs=None, obs_P='P_F', obs_swc='SWC_F_MDS_1', scale_obs_swc=True):
 
+    # set z=0 if z is none
+
     obs_swc = obs[obs_swc]
 
     # If obs are in % instead of fraction
@@ -21,7 +39,7 @@ def plot_precip_and_swc(model_ds, z=None, obs=None, obs_P='P_F', obs_swc='SWC_F_
 
     precip_plot = obs[obs_P].hvplot.line(xlim=(model_ds.time.min().values, model_ds.time.max().values))
     swc_obs_plot = obs_swc.hvplot.line(xlim=(model_ds.time.min().values, model_ds.time.max().values))
-    swc_model_plot = model_ds.THETA.sel(z=z).hvplot.line(x='time', label='model SWC')
+    swc_model_plot = model_ds.THETA.sel(z=z, method='nearest').hvplot.line(x='time', label='model SWC')
     swc_plot = (swc_obs_plot * swc_model_plot).opts(ylim=(0,0.3))
     return (precip_plot + swc_plot).cols(1)
 
@@ -34,21 +52,33 @@ def plot_precip_swc(res, swc_var='SWC', p_var='P_F', swc_lim=(0, 50), p_lim=(0, 
     return overlay.opts(multi_y=True, xlim=(res.canopy1d.time.min().values, res.canopy1d.time.max().values), **kwargs).redim(TIMESTAMP='time')
 
 
-def plot_precip_swc_vpd(res, swc_var='SWC', p_var='P_F', irr_var=None, vpd_var='VPD_F', swc_lim=(0, None), p_lim=(0, None), vpd_lim=(0,None), swc_label="SWC (%)", p_label="P (mm)", p_ax=None, **kwargs):
+def plot_precip_swc_vpd(obs_df,
+                        swc_var='SWC',
+                        p_var='P_F',
+                        irr_var=None,
+                        vpd_var='VPD_F',
+                        tlim=(None, None),
+                        swc_lim=(0, None),
+                        p_lim=(0, None),
+                        vpd_lim=(0,None),
+                        swc_label="SWC",
+                        p_label="P (mm)",
+                        scale_obs_swc=True,
+                        p_ax=None,
+                        show_xlabel=True,
+                        **kwargs):
 
-    if isinstance(res, Results):
-        df=res.obs
-        tmin = res.canopy1d.time.min().values
-        tmax = res.canopy1d.time.max().values
-    else:
-        df=res
-        tmin = df.index.min()
-        tmax = df.index.max()
+    df = obs_df.copy()
+
+    # If obs are in % instead of fraction
+    if scale_obs_swc:
+        df[swc_var] = df[swc_var] / 100
 
     rename = {df.index.name: 'time'}
+    df.index.name = 'time'
 
     p_plot = hv.Curve(df, kdims=df.index.name, vdims=p_var, label="P (mm)").opts(ylim=p_lim, invert_yaxis=True, color="#47a5e3", ylabel=p_label, yaxis=p_ax, **kwargs)
-    swc_plot = hv.Curve(df, kdims=df.index.name, vdims=swc_var, label="SWC").opts(color="k", ylim=swc_lim, ylabel=swc_label, **kwargs)
+    swc_plot = hv.Curve(df, kdims=df.index.name, vdims=[swc_var], label="SWC").opts(ylim=swc_lim, ylabel=swc_label, color='k', **kwargs)
 
     if irr_var is not None:
         irr_plot = hv.Curve(df, kdims=df.index.name, vdims=irr_var, label="Irrigation (mm)").opts(ylim=p_lim, invert_yaxis=True, color="#0b5ca3", yaxis=None, **kwargs)
@@ -59,17 +89,30 @@ def plot_precip_swc_vpd(res, swc_var='SWC', p_var='P_F', irr_var=None, vpd_var='
         overlay =  swc_plot * p_plot * irr_plot * vpd_plot
     else:
         overlay =  swc_plot * p_plot * vpd_plot
-    return overlay.opts(multi_y=True, xlim=(tmin, tmax)).redim(**rename)
+
+    plot_opts = dict(multi_y=True, xlim=tlim)
+    if not show_xlabel:
+        plot_opts['hooks'] = [xlabel_off]
+        plot_opts['xlabel'] = ''
+    return overlay.opts(**plot_opts).redim(**rename)
 
 
-def plot_sap(res, obs_df, tree_name=None, **kwargs):
-    canopy1d = res.canopy1d
-    df = res.obs[obs_df]
-    if tree_name is None:
-        tree_name = res.cfg.species
-    rename = {df.index.name: 'time'}
-    mod_sap = hv.Curve(canopy1d, kdims='time', vdims='trans', label="Model sapflow").opts(**kwargs)
-    nhl = hv.Curve(canopy1d, kdims='time', vdims='nhl', label="Potential Transp.").opts(**kwargs)
-    obs_sap = hv.Curve(df, kdims='TIMESTAMP', vdims=tree_name, label="Observed sapflow").redim(**rename).opts(**kwargs)
+def plot_sap(mod_ds, obs_df, obs_var=None, show_xlabel=True, **kwargs):
+    df = obs_df.copy()
+    df.index.name = 'time'
+    mod_sap = hv.Curve(mod_ds, kdims='time', vdims='trans', label="Model sapflow").opts(**kwargs)
+    nhl = hv.Curve(mod_ds, kdims='time', vdims='nhl', label="Potential Transp.").opts(**kwargs)
+    obs_sap = hv.Curve(df, kdims='time', vdims=obs_var, label="Observed sapflow").opts(**kwargs)
 
-    return (nhl * mod_sap * obs_sap).opts(ylabel="Sapflow (cm3 hr-1)", responsive=True,)
+    plot_opts={}
+    if not show_xlabel:
+        plot_opts['hooks'] = [xlabel_off]
+        plot_opts['xlabel'] = ''
+
+    return (nhl * mod_sap * obs_sap).opts(ylabel="Sapflow (cm3 hr-1)", **plot_opts, **plot_kw)
+
+def plot_stemwp(ds, ds_daily, tlim=(None,None), **kwargs):
+    stem_wp = hv.Curve(ds, kdims='time', vdims='H', label="Stem water potential").opts(**kwargs)
+    stem_wp_daily = hv.Curve(ds_daily, kdims='time', vdims='H_min', label="Daily min stem water pot.").opts(**kwargs)
+
+    return (stem_wp * stem_wp_daily).opts(ylabel="Stem water potential", xlim=tlim, **plot_kw)
